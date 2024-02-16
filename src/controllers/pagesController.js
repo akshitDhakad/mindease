d09acb18d2adbb8125d61f4c7e91f8850f3917a6
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-02-16 09:15:24 +0530

    Update page titles and add new pages

--- src/controllers/pagesController.js
+++ src/controllers/pagesController.js
@@ -2,7 +2,13 @@ exports.homePage =async (req, res) => {
   res.render("index", { title: "Home" });
 }
 exports.bookNow =async (req, res) => {
-  res.render("bookNow", { title: "Home" });
+  res.render("bookNow", { title: "Book Now" });
+}
+exports.servicePage =async (req, res) => {
+  res.render("service", { title: "Servers" });
+}
+exports.blogPage =async (req, res) => {
+  res.render("blog", { title: "Blogs" });
 }
 exports.loginPage =async (req, res) => {
   res.render("login", { title: "Login" });
